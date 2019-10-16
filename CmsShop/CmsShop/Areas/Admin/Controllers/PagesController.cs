d6447f23f5440383da984d2ddb7926a106c091f1
Wojciech Kaliszewski <D534549@euc.ppg.com>
2019-10-16 11:51:40 +0200

    Add functionality to add page to base

--- CmsShop/CmsShop/Areas/Admin/Controllers/PagesController.cs
+++ CmsShop/CmsShop/Areas/Admin/Controllers/PagesController.cs
@@ -1,8 +1,8 @@
-﻿using CmsShop.Areas.Admin.Models.Data;
-using CmsShop.Areas.Admin.ViewModels.Pages;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
+using CmsShop.Areas.Admin.Models.Data;
+using CmsShop.Areas.Admin.ViewModels.Pages;
 
 namespace CmsShop.Areas.Admin.Controllers
 {
@@ -13,7 +13,7 @@ namespace CmsShop.Areas.Admin.Controllers
         {
             // Deklaracja listy PageVM
             List<PageVM> pagesList;
-            
+
             using (Db db = new Db())
             {
                 // Inicjalizacja listy
@@ -21,7 +21,65 @@ namespace CmsShop.Areas.Admin.Controllers
             }
 
             //Zwracamy strony do widoku
-            return View(pagesList);
+            return this.View(pagesList);
+        }
+
+        // GET Admin/Pages
+        [HttpGet]
+        public ActionResult AddPage()
+        {
+            return this.View();
+        }
+
+        // Post Admin/Pages
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+            // Sprawdzanie model state
+
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            using (Db db = new Db())
+            {
+                string slug;
+                // Inicjalizacja PageDTO
+                PageDTO dto = new PageDTO();
+
+                // Jełsi Slug nie jest wypełniony zastępujemy go tytułem
+
+                if (string.IsNullOrWhiteSpace(model.Slug))
+                {
+                    slug = model.Title.Replace(" ", "-").ToLower();
+                }
+                else
+                {
+                    slug = model.Slug.Replace(" ", "-").ToLower();
+                }
+
+                // Zapobiegamy dodaniu takiej samej nazwy strony
+                if (db.Pages.Any(x => x.Title == model.Title) || db.Pages.Any(x => x.Slug == slug))
+                {
+                    this.ModelState.AddModelError("", "ten tytuł lub adress już istnieje");
+                    return this.View(model);
+                }
+
+                dto.Title = model.Title;
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSideBar = model.HasSideBar;
+                dto.Sorting = 1000;
+
+                // Zapis DTO
+                db.Pages.Add(dto);
+                db.SaveChanges();
+            }
+
+            this.TempData["SM"] = "Dodałeś nową stronęęęę";
+
+            return RedirectToAction("AddPage");
         }
     }
 }
--- CmsShop/CmsShop/Areas/Admin/ViewModels/Pages/PageVM.cs
+++ CmsShop/CmsShop/Areas/Admin/ViewModels/Pages/PageVM.cs
@@ -26,12 +26,16 @@ namespace CmsShop.Areas.Admin.ViewModels.Pages
         public int Id { get; set; }
         [Required]
         [StringLength(50, MinimumLength = 3)]
+        [Display(Name = "Tytuł strony")]
         public string Title { get; set; }
+        [Display(Name = "Adres strony")]
         public string Slug { get; set; }
         [Required]
         [StringLength(int.MaxValue, MinimumLength = 3)]
+        [Display(Name = "Zawartość strony")]
         public string Body { get; set; }
         public int Sorting { get; set; }
+        [Display(Name = "Pasek boczny")]
         public bool HasSideBar { get; set; }
     }
 }
